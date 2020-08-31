e86cda81488c5bda2cca29ada4c96cedb6b075c9
Briana Kubik <bckubik@umass.edu>
2020-08-31 11:54:11 -0400

    Uploading Lab1 files

---
+++ Lab1_BrianaKubik.R
@@ -0,0 +1,33 @@
+###################################################
+### Lab 1- Intro to R and Reproducible Research ###
+###################################################
+
+### Exercise 1 ###
+x = 3
+y = 15
+
+#Sum
+sum(x,y)
+
+#Difference
+x - y
+
+#Product
+x * y
+
+#Quotient
+x / y
+
+### Exercise 2 ###
+x = 2
+y = 3
+x^5 > y^4
+
+### Exercise 3 ###
+vec = c(22, 62, 148, 43, 129)
+sum_vec = sum(vec)
+vec / sum_vec
+
+### Exercise 4 ###
+vec2 = c("A", "T", "C", "G")
+sort(vec2)
